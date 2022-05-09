1f2686dc61d8a863fcaac594b11c79f966eccb65
Kasper Nyhus Kaae <kanyhus@gmail.com>
2022-05-09 11:13:43 +0200

    working 9bit with ML

--- rmt_uart.c
+++ rmt_uart.c
@@ -33,12 +33,15 @@ typedef struct {
 
 static rmt_uart_contex_t rmt_uart_contex[RMT_UART_NUM_MAX] = {0};
 
-static int convert_byte_to_items(rmt_uart_contex_t* ctx, uint8_t byte)
+static int convert_byte_to_items(rmt_uart_contex_t* ctx, uint16_t byte)
 {
     rmt_uart_contex_tx_t* rtc = &ctx->rmt_uart_contex_tx;
-    uint16_t data = (byte << 1) | (1 << 9);
+    size_t total_bits = ctx->rmt_uart_config.data_bits == RMT_UART_DATA_8_BITS ? 9 : 11;
+    uint16_t stop_bit = ctx->rmt_uart_config.data_bits == RMT_UART_DATA_8_BITS ? 0x200 : 0xC00;
+    uint16_t data = (byte << 1) | stop_bit;
+    ESP_LOGD("rmt convert","total_bits:%d, stop_bit:%d, data: %.4X",total_bits, stop_bit, data);
 
-    for (int i = 0; i < 9; i += 2)
+    for (int i = 0; i < total_bits; i += 2)
     {
         rmt_item32_t* item = &rtc->items[rtc->item_index];
         item->duration0 = ctx->rmt_bit_len;
@@ -46,15 +49,16 @@ static int convert_byte_to_items(rmt_uart_contex_t* ctx, uint8_t byte)
         item->level0 = (data >> i) ^ 1;
         item->level1 = (data >> (i + 1)) ^ 1;
         rtc->item_index++;
-        if (rtc->item_index >= (ctx->rmt_uart_config.buffer_size / sizeof(rmt_item32_t)))
+        if (rtc->item_index >= ctx->rmt_uart_config.tx_items_buffer_size)
         {
-            ESP_LOGE(TAG, "DATA TOO LONG");
+            ESP_LOGE(TAG, "DATA TOO LONG - increase tx_items_buffer_size");
             return -1;
         }
         ESP_LOGD(TAG, "\trmt tx item %02d: duration0: %d level0: %d  duration1: %d level1: %d", rtc->item_index, item->duration0, item->level0, item->duration1, item->level1);
     }
     return 0;
 }
+
 
 static int convert_data_to_items(rmt_uart_contex_t* ctx, const uint8_t* data, uint16_t len)
 {
@@ -63,10 +67,17 @@ static int convert_data_to_items(rmt_uart_contex_t* ctx, const uint8_t* data, ui
     
     for (int i = 0; i < len; ++i)
     {
-        if (convert_byte_to_items(ctx, data[i])) return -1;
+        if(ctx->rmt_uart_config.data_bits == RMT_UART_DATA_9_BITS) {
+            if(convert_byte_to_items(ctx, (data[i]<<8)|data[i+1])) return -1;
+            i++;
+        }
+        else {
+            if (convert_byte_to_items(ctx, (uint16_t)data[i])) return -1;
+        }
     }
     return rtc->item_index;
 }
+
 
 static unsigned int round_closest(unsigned int dividend, unsigned int divisor)
 {
@@ -80,7 +91,7 @@ static void fill_bits(rmt_uart_contex_t* ctx, uint32_t duration, uint32_t level)
     // all remaining bits are high
     if (rmt_bit_numbers == 0 && level)
     {
-        rmt_bit_numbers = 10 - rrc->bit_num;
+        rmt_bit_numbers = 11 - rrc->bit_num;
     }
 
     int from = rrc->bit_num;
@@ -93,74 +104,79 @@ static void fill_bits(rmt_uart_contex_t* ctx, uint32_t duration, uint32_t level)
             return;
         }
         rrc->bit_num++;
-        if (rrc->bit_num == 10 && !level)
+        if (rrc->bit_num == 11 && !level)
         {
            ESP_LOGE(TAG, "not a stop bit byte_num=%d", rrc->byte_num);
            return;
         }
         rrc->raw_data |= (level << j);
-        if (rrc->bit_num == 10)
+        if (rrc->bit_num == 11)
         {
             rrc->raw_data >>= 1;
-            rrc->raw_data &= 0x00FF;
-            rrc->bytes[rrc->byte_num] = rrc->raw_data;
+            rrc->raw_data &= 0x01FF;
+            rrc->bytes[rrc->byte_num] = rrc->raw_data >> 8;
+            rrc->bytes[rrc->byte_num+1] = rrc->raw_data & 0xFF;
             ESP_LOGD(TAG, "\trmt rx data=%d", rrc->raw_data);
-            rrc->byte_num++;
+            rrc->byte_num+=2;
             rrc->bit_num = 0;
             rrc->raw_data = 0;
         }
     }
 }
 
-esp_err_t rmt_uart_init(rmt_uart_port_t uart_num, const rmt_uart_config_t* uart_config)
+esp_err_t rmt_uart_init(rmt_uart_port_t uart_num_rx, rmt_uart_port_t uart_num_tx, const rmt_uart_config_t* rmt_uart_config)
 {
-    const int RMT_DIV = APB_CLK_FREQ / 50 / uart_config->baud_rate;
+    const int RMT_DIV = APB_CLK_FREQ / 50 / rmt_uart_config->baud_rate;
     const int RMT_TICK = APB_CLK_FREQ / RMT_DIV;
-    ESP_LOGI(TAG, "baud=%d rmt_div=%d rmt_tick=%d", uart_config->baud_rate, RMT_DIV, RMT_TICK);
+    ESP_LOGI(TAG, "baud=%d rmt_div=%d rmt_tick=%d", rmt_uart_config->baud_rate, RMT_DIV, RMT_TICK);
 
-    ESP_RETURN_ON_FALSE((uart_num < RMT_UART_NUM_MAX), ESP_FAIL, TAG, "uart_num error");
-    ESP_RETURN_ON_FALSE((uart_config), ESP_FAIL, TAG, "uart_config error");
-    ESP_RETURN_ON_FALSE(((10UL * RMT_TICK / uart_config->baud_rate) < 0xFFFF), ESP_FAIL, TAG, "rmt tick too long, reconfigure 'RMT_DIV'");
-    ESP_RETURN_ON_FALSE(((RMT_TICK / uart_config->baud_rate) > 49), ESP_FAIL, TAG, "rmt tick too long, reconfigure 'RMT_DIV'");
-    ESP_RETURN_ON_FALSE(((RMT_TICK / uart_config->baud_rate / 2) < 0xFF), ESP_FAIL, TAG, "baud rate too slow, reconfigure 'RMT_DIV'");
+    ESP_RETURN_ON_FALSE((uart_num_rx < RMT_UART_NUM_MAX), ESP_FAIL, TAG, "uart_num error");
+    ESP_RETURN_ON_FALSE((rmt_uart_config), ESP_FAIL, TAG, "uart_config error");
+    ESP_RETURN_ON_FALSE(((10UL * RMT_TICK / rmt_uart_config->baud_rate) < 0xFFFF), ESP_FAIL, TAG, "rmt tick too long, reconfigure 'RMT_DIV'");
+    ESP_RETURN_ON_FALSE(((RMT_TICK / rmt_uart_config->baud_rate) > 49), ESP_FAIL, TAG, "rmt tick too long, reconfigure 'RMT_DIV'");
+    ESP_RETURN_ON_FALSE(((RMT_TICK / rmt_uart_config->baud_rate / 2) < 0xFF), ESP_FAIL, TAG, "baud rate too slow, reconfigure 'RMT_DIV'");
 
-    uint16_t bit_len = RMT_TICK / uart_config->baud_rate;
-    rmt_uart_contex[uart_num].rmt_bit_len = bit_len;
-    memcpy(&rmt_uart_contex[uart_num].rmt_uart_config, uart_config, sizeof(rmt_uart_config_t));
+    uint16_t bit_len = RMT_TICK / rmt_uart_config->baud_rate;
+    rmt_uart_contex[uart_num_rx].rmt_bit_len = bit_len;
+    rmt_uart_contex[uart_num_tx].rmt_bit_len = bit_len;
+    memcpy(&rmt_uart_contex[uart_num_rx].rmt_uart_config, rmt_uart_config, sizeof(rmt_uart_config_t));
+    memcpy(&rmt_uart_contex[uart_num_tx].rmt_uart_config, rmt_uart_config, sizeof(rmt_uart_config_t));
 
-    if (uart_config->mode == RMT_UART_MODE_RX_ONLY || uart_config->mode == RMT_UART_MODE_TX_RX)
+    if (rmt_uart_config->mode == RMT_UART_MODE_RX_ONLY || rmt_uart_config->mode == RMT_UART_MODE_TX_RX)
     {
-        rmt_config_t rmt_config_rx = RMT_DEFAULT_CONFIG_RX(uart_config->rx_io_num, uart_num);
+        rmt_config_t rmt_config_rx = RMT_DEFAULT_CONFIG_RX(rmt_uart_config->rx_io_num, rmt_uart_config->rmt_rx_channel);
         rmt_config_rx.clk_div = RMT_DIV;
-        rmt_config_rx.mem_block_num = (uart_config->mode == RMT_UART_MODE_RX_ONLY) ? 2 : 1;
-        rmt_config_rx.rx_config.idle_threshold = 10 * bit_len;
+        rmt_config_rx.mem_block_num = (rmt_uart_config->mode == RMT_UART_MODE_RX_ONLY) ? 2 : 1;
+        rmt_config_rx.rx_config.idle_threshold = 15 * bit_len;
         rmt_config_rx.rx_config.filter_en = true;
         rmt_config_rx.rx_config.filter_ticks_thresh = bit_len / 2;
         ESP_ERROR_CHECK(rmt_config(&rmt_config_rx));
-        ESP_ERROR_CHECK(rmt_driver_install(rmt_config_rx.channel, uart_config->buffer_size, 0));
-        rmt_uart_contex[uart_num].rmt_config_rx = rmt_config_rx;
-        rmt_get_ringbuf_handle(rmt_config_rx.channel, &rmt_uart_contex[uart_num].rmt_uart_contex_rx.rb);
-        ESP_RETURN_ON_FALSE((rmt_uart_contex[uart_num].rmt_uart_contex_rx.rb), ESP_FAIL, TAG, "rmt ringbuffer is null");
-#if CONFIG_SPIRAM_USE_MALLOC
-        rmt_uart_contex[uart_num].rmt_uart_contex_tx.items = heap_caps_calloc(1, uart_config->buffer_size, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-#else
-        rmt_uart_contex[uart_num].rmt_uart_contex_tx.items = calloc(1, uart_config->buffer_size);
-#endif
-        rmt_rx_start(rmt_uart_contex[uart_num].rmt_config_rx.channel, true);
+        ESP_ERROR_CHECK(rmt_driver_install(rmt_config_rx.channel, rmt_uart_config->rx_buffer_size, 0));
+        rmt_uart_contex[uart_num_rx].rmt_config_rx = rmt_config_rx;
+        rmt_get_ringbuf_handle(rmt_config_rx.channel, &rmt_uart_contex[uart_num_rx].rmt_uart_contex_rx.rb);
+        ESP_RETURN_ON_FALSE((rmt_uart_contex[uart_num_rx].rmt_uart_contex_rx.rb), ESP_FAIL, TAG, "rmt ringbuffer is null");
+        rmt_rx_start(rmt_uart_contex[uart_num_rx].rmt_config_rx.channel, true);
     }
 
-    if (uart_config->mode == RMT_UART_MODE_TX_ONLY || uart_config->mode == RMT_UART_MODE_TX_RX)
+    if (rmt_uart_config->mode == RMT_UART_MODE_TX_ONLY || rmt_uart_config->mode == RMT_UART_MODE_TX_RX)
     {
-        rmt_config_t rmt_config_tx = RMT_DEFAULT_CONFIG_TX(uart_config->tx_io_num, uart_num + 1);
+        rmt_config_t rmt_config_tx = RMT_DEFAULT_CONFIG_TX(rmt_uart_config->tx_io_num, rmt_uart_config->rmt_tx_channel);
         rmt_config_tx.tx_config.carrier_en = false;
         rmt_config_tx.clk_div = RMT_DIV;
         rmt_config_tx.flags = RMT_CHANNEL_FLAGS_INVERT_SIG;
         ESP_ERROR_CHECK(rmt_config(&rmt_config_tx));
-        ESP_ERROR_CHECK(rmt_driver_install(rmt_config_tx.channel, uart_config->buffer_size, 0));
-        rmt_uart_contex[uart_num].rmt_config_tx = rmt_config_tx;
+        ESP_ERROR_CHECK(rmt_driver_install(rmt_config_tx.channel, 0, 0));
+        rmt_uart_contex[uart_num_tx].rmt_config_tx = rmt_config_tx;
+
+        #if CONFIG_SPIRAM_USE_MALLOC
+        rmt_uart_contex[uart_num_tx].rmt_uart_contex_tx.items = heap_caps_calloc(1, rmt_uart_config->buffer_size, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+        #else
+        rmt_uart_contex[uart_num_tx].rmt_uart_contex_tx.items = calloc(rmt_uart_config->tx_items_buffer_size, sizeof(rmt_item32_t));
+        #endif
     }
 
-    rmt_uart_contex[uart_num].configured = true;
+    rmt_uart_contex[uart_num_rx].configured = true;
+    rmt_uart_contex[uart_num_tx].configured = true;
 
     return ESP_OK;
 }
@@ -172,10 +188,6 @@ esp_err_t rmt_uart_write_bytes(rmt_uart_port_t uart_num, const uint8_t* data, si
     ESP_RETURN_ON_FALSE((ctx->rmt_uart_config.mode != RMT_UART_MODE_RX_ONLY), ESP_FAIL, TAG, "uart RX only");
     rmt_uart_contex_tx_t* rtc = &ctx->rmt_uart_contex_tx;
     if (convert_data_to_items(ctx, data, size) < 0) return ESP_FAIL;
-    // printf("rmt tx ");
-    // for (int i = 0; i < size; ++i)
-    //     printf("%d ", data[i]);
-    // printf("\n");
     return rmt_write_items(ctx->rmt_config_tx.channel, ctx->rmt_uart_contex_tx.items, rtc->item_index, true);
 }
 
@@ -190,7 +202,7 @@ int rmt_uart_read_bytes(rmt_uart_port_t uart_num, uint8_t* buf, size_t size, Tic
     rrc->bit_num = 0;
     size_t length = 0;
     rmt_item32_t* items = NULL;
-
+    
     items = (rmt_item32_t*)xRingbufferReceive(rrc->rb, &length, ticks_to_wait);
     if (items) {
         for (size_t i = 0; i < length / 4; ++i)
@@ -202,10 +214,6 @@ int rmt_uart_read_bytes(rmt_uart_port_t uart_num, uint8_t* buf, size_t size, Tic
         vRingbufferReturnItem(rrc->rb, (void*)items);
 
         ESP_LOGD(TAG, "\trx is complete byte_num=%d", rrc->byte_num);
-        // printf("rmt rx ");
-        // for (int i = 0; i < rrc->byte_num; ++i)
-        //     printf("%d ", rrc->bytes[i]);
-        // printf("\n\n");
     }
     return rrc->byte_num;
 }
--- rmt_uart.h
+++ rmt_uart.h
@@ -10,13 +10,18 @@ extern "C" {
 #include "freertos/FreeRTOS.h"
 #include "soc/soc_caps.h"
 #include "hal/gpio_types.h"
+#include "driver/rmt.h"
 
 #define RMT_UART_NUM_0             (0) /*!< RMT UART port 0 */
 #define RMT_UART_NUM_1             (1) /*!< RMT UART port 1 */
 #if SOC_RMT_CHANNELS_PER_GROUP > 4
 #define RMT_UART_NUM_2             (2) /*!< RMT UART port 2 */
-#define RMT_UART_NUM_3             (3) /*!< RMT UART port 2 */
-#define RMT_UART_NUM_MAX           (4)
+#define RMT_UART_NUM_3             (3) /*!< RMT UART port 3 */
+#define RMT_UART_NUM_4             (4) /*!< RMT UART port 4 */
+#define RMT_UART_NUM_5             (5) /*!< RMT UART port 5 */
+#define RMT_UART_NUM_6             (6) /*!< RMT UART port 6 */
+#define RMT_UART_NUM_7             (7) /*!< RMT UART port 7 */
+#define RMT_UART_NUM_MAX           (8)
 #else
 #define RMT_UART_NUM_MAX           (2)
 #endif
@@ -36,7 +41,7 @@ typedef enum {
 } rmt_uart_word_length_t;
 
 typedef enum {
-    RMT_UART_STOP_BITS_1   = 0x1,  /*!< stop bit: 1bit*/
+    RMT_UART_STOP_BITS_1   = 0x1,    /*!< stop bit: 1bit*/
     RMT_UART_STOP_BITS_MAX = 0x2,
 } rmt_uart_stop_bits_t;
 
@@ -52,10 +57,13 @@ typedef struct {
     rmt_uart_stop_bits_t stop_bits;       /*!< UART stop bits*/
     gpio_num_t tx_io_num;
     gpio_num_t rx_io_num;
-    size_t buffer_size;
+    size_t rx_buffer_size;
+    size_t tx_items_buffer_size;
+    rmt_channel_t rmt_tx_channel;
+    rmt_channel_t rmt_rx_channel;
 } rmt_uart_config_t;
 
-esp_err_t rmt_uart_init(rmt_uart_port_t uart_num, const rmt_uart_config_t* uart_config);
+esp_err_t rmt_uart_init(rmt_uart_port_t uart_num_rx, rmt_uart_port_t uart_num_tx, const rmt_uart_config_t* uart_config);
 esp_err_t rmt_uart_write_bytes(rmt_uart_port_t uart_num, const uint8_t* data, size_t size);
 esp_err_t rmt_uart_read_bytes(rmt_uart_port_t uart_num, uint8_t* buf, size_t size, TickType_t ticks_to_wait);
 esp_err_t rmt_uart_deinit(rmt_uart_port_t uart_num);
